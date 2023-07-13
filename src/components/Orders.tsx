1de843088f95136865d96c0bbc7b94ec2f29c0de
Himanshu Pandey <himanshu21pandey@gmail.com>
2023-07-13 22:19:03 +0530

    Multiple Form Input Working

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,47 +1,89 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "@/components/ui/use-toast";
+import { Input } from "./ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+const FormSchema = z.object({
+  index: z.string({
+    required_error: "Please select an index.",
+  }),
+  trigger: z.coerce.number({
+    required_error: "Please enter a trigger level.",
   }),
 });
 
-const Orders = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+export default function Orders() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      trigger: 0,
     },
   });
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
   }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
         <FormField
           control={form.control}
-          name="username"
+          name="index"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>New Order</FormLabel>
+              <FormLabel>Index</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select Index" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="BANKNIFTY">BANKNIFTY</SelectItem>
+                  <SelectItem value="NIFTY">NIFTY</SelectItem>
+                  <SelectItem value="FINNIFTY">FINNIFTY</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="trigger"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Trigger</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input type="number" placeholder="Trigger Level" {...field} />
               </FormControl>
-              <FormDescription>Submit new order.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -50,6 +92,4 @@ const Orders = () => {
       </form>
     </Form>
   );
-};
-
-export default Orders;
+}
