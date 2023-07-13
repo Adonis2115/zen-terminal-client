17049c0840cb5373016b917875b1404a65a65eba
Himanshu Pandey <himanshu21pandey@gmail.com>
2023-07-14 00:16:06 +0530

    Form Complete with errors

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -18,7 +18,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Toggle } from "@/components/ui/toggle";
 import { toast } from "@/components/ui/use-toast";
+import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 import { Input } from "./ui/input";
 
 const FormSchema = z.object({
@@ -28,16 +31,36 @@ const FormSchema = z.object({
   trigger: z.coerce.number({
     required_error: "Please enter a trigger level.",
   }),
+  action: z.string({
+    required_error: "Please select candle stick action.",
+  }),
+  strike: z.coerce.number({
+    required_error: "Please enter a strike price.",
+  }),
+  expiry: z.coerce.date({
+    required_error: "Please select expiry date.",
+  }),
 });
-
+enum Index {
+  BANKNIFTY = "BANKNIFTY",
+  NIFTY = "NIFTY",
+  FINNIFTY = "FINNIFTY",
+}
 export default function Orders() {
+  const [expiries] = useState(["2023-06-20", "2023-06-24", "2023-06-30"]);
+  const [lots, setLots] = useState(1);
+  const [indexs] = useState<Index[]>([
+    Index.BANKNIFTY,
+    Index.NIFTY,
+    Index.FINNIFTY,
+  ]);
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       trigger: 0,
+      strike: 0,
     },
   });
-
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
     toast({
@@ -66,9 +89,11 @@ export default function Orders() {
                   </SelectTrigger>
                 </FormControl>
                 <SelectContent>
-                  <SelectItem value="BANKNIFTY">BANKNIFTY</SelectItem>
-                  <SelectItem value="NIFTY">NIFTY</SelectItem>
-                  <SelectItem value="FINNIFTY">FINNIFTY</SelectItem>
+                  {indexs.map((index, i) => (
+                    <SelectItem key={i} value={index}>
+                      {index}
+                    </SelectItem>
+                  ))}
                 </SelectContent>
               </Select>
               <FormMessage />
@@ -88,6 +113,99 @@ export default function Orders() {
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="action"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Action</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select Action" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="crossup">Cross Up</SelectItem>
+                  <SelectItem value="crossdown">Cross Down</SelectItem>
+                  <SelectItem value="touch">Touch</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Toggle aria-label="Toggle italic">
+          <p>CE</p>
+        </Toggle>
+        <Toggle aria-label="Toggle italic">
+          <p>PE</p>
+        </Toggle>
+        <Toggle aria-label="Toggle italic">
+          <p>Buy</p>
+        </Toggle>
+        <Toggle aria-label="Toggle italic">
+          <p>Sell</p>
+        </Toggle>
+        <FormField
+          control={form.control}
+          name="strike"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Strike Price</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="Strike Price" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="expiry"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Expiry Date</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value as unknown as string}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Expiry Date" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {expiries.map((expiry, index) => (
+                    <SelectItem key={index} value={expiry}>
+                      {expiry}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div>
+          <Button
+            type="button"
+            variant="outline"
+            size="icon"
+            onClick={() => setLots((prev) => (prev > 1 ? prev - 1 : prev))}
+          >
+            <Minus className="h-4 w-4" />
+          </Button>
+          <span className="mr-2 ml-2">{lots}</span>
+          <Button
+            type="button"
+            variant="outline"
+            size="icon"
+            onClick={() => setLots((prev) => prev + 1)}
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
